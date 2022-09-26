29c95bc090de4a522cb980cb0823989b57af74da
blank0211 <wkgchoh@gmail.com>
2022-09-26 14:38:19 +0530

    Drew player. Implemented movement (glitchy)

---
+++ main.py
@@ -0,0 +1,75 @@
+import sys
+import pygame as pg
+vec2 = pg.math.Vector2
+
+# Colours
+black = (0, 0, 0)
+blue1 = pg.Color("#197bd2")
+red = pg.Color("#f04c64")
+
+# Display
+width, height = 640, 480
+FPS = 60
+win = pg.display.set_mode((width, height))
+pg.display.set_caption("friction")
+clock = pg.time.Clock()
+
+# Sprites
+class Player(pg.sprite.Sprite):
+    def __init__(self, color, size, pos):
+        self.pos = vec2(pos)
+        self.vel = vec2(0, 0)
+        self.acc = vec2(0, 0)
+        self.fric = 0.98
+        self.max_speed = 8
+
+        self.image = pg.Surface(size)
+        self.rect = self.image.get_rect(center=pos)
+        self.image.fill(red)
+
+    def update(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_RIGHT]:
+            self.acc.x += 1
+        elif keys[pg.K_LEFT]:
+            self.acc.x -= 1
+
+        self.vel += self.acc
+        self.vel *= self.fric
+        self.pos += self.vel
+
+        self.rect.center = (round(self.pos.x), round(self.pos.y))
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
+
+
+p1 = Player(red, (18, 36), (width//2, height//2))
+
+# Game Loop
+def main():
+    pg.init()
+
+    running = True
+    while running:
+        # Handle events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        # Update & draw sprites
+        p1.update()
+        win.fill(black) # Clear background
+        p1.draw(win)
+
+        # Update Display
+        pg.display.update()
+        clock.tick(FPS)
+
+    # Post loop
+    pg.quit()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
